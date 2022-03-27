4475ba4a0c1026e41afb9b9c433bf13f1dca227d
OvidiuDragos11 <94932671+OvidiuDragos11@users.noreply.github.com>
2022-03-27 17:14:54 +0300

    Update QuizGameProject.py

--- QuizGameProject.py
+++ QuizGameProject.py
@@ -11,7 +11,7 @@ print("Okay,let's play!")
 
 score = 0
 
-answer = input("What's the capital of United Kingdom? ").upper()
+answer = input("1.What's the capital of United Kingdom? ").upper()
 
 if answer == "LONDON":
     print("Correct!")
@@ -19,7 +19,7 @@ if answer == "LONDON":
 else:
     print("Incorrect!")
 
-answer = input("What's the captial of Spain? ").upper()
+answer = input("2.What's the captial of Spain? ").upper()
 
 if answer == "MADRID":
     print("Correct!")
@@ -28,7 +28,7 @@ else:
     print("Incorrect!")
 
 
-answer = input("Which is the biggest planet in our Solar System? ").upper()
+answer = input("3.Which is the biggest planet in our Solar System? ").upper()
 
 if answer == "JUPITER":
     print("Correct!")
@@ -36,7 +36,7 @@ if answer == "JUPITER":
 else:
     print("Incorrect!")
 
-answer = int(input ("How many planets are in our Solar System? "))
+answer = int(input ("4.How many planets are in our Solar System? "))
 
 if answer == 9:
     print("That's correct!")
@@ -44,42 +44,42 @@ if answer == 9:
 else :
     print("That's incorrect!")
 
-answer = input("What RAM stands for? ").upper()
+answer = input("5.What RAM stands for? ").upper()
 if answer == "RANDOM ACCESS MEMORY":
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("What CPU stands for? ").upper()
+answer = input("6.What CPU stands for? ").upper()
 if answer =="CENTRAL PROCESSING UNIT":
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("Who is the biggest manufacturer of CPUs? ").upper()
+answer = input("7.Who is the biggest manufacturer of CPUs? ").upper()
 if answer == "INTEL":
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("What GPU stands for? ").upper()
+answer = input("8.What GPU stands for? ").upper()
 if answer =="GRAPHIC PROCESSING UNIT":
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("Who is the biggest manufacturer of GPUs ? ").upper()
+answer = input("9.Who is the biggest manufacturer of GPUs ? ").upper()
 if answer =="NVIDIA":
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("Who won the european championship at football in 2021 ? ").upper()
+answer = input("10.Who won the european championship at football in 2021 ? ").upper()
 
 if answer == "ITALY":
     print("Correct")
